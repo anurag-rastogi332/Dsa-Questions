775e15874959db772d6529e38a54c0e7c91e4464
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-03-19 21:49:34 +0530

    Stacks

---
+++ Patterns-printing/Half Diamond Numeric Pattern with Star Boundary.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+🧪 Examples
+🔹 Example 1
+
+Input
+
+N = 3
+
+Output
+
+*
+* 1 *
+* 1 2 1 *
+* 1 2 3 2 1 *
+* 1 2 1 *
+* 1 *
+*
+🔹 Example 2
+
+Input
+
+N = 4
+
+Output
+
+*
+* 1 *
+* 1 2 1 *
+* 1 2 3 2 1 *
+* 1 2 3 4 3 2 1 *
+* 1 2 3 2 1 *
+* 1 2 1 *
+* 1 *
+*
+🔹 Example 3
+
+Input
+
+N = 2
+
+Output
+
+*
+* 1 *
+* 1 2 1 *
+* 1 *
+*
+🔹 Example 4
+
+Input
+
+N = 1
+
+Output
+
+*
+* 1 *
+*
+
+*/
+
