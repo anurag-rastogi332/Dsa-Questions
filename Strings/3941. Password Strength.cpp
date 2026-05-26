ae1472a73521be9e1aa78bfbfe47545d4a4799e4
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-05-27 01:28:48 +0530

    Strings

--- Strings/3941. Password Strength.cpp
+++ Strings/3941. Password Strength.cpp
@@ -1,8 +1,47 @@
-#include<iostream>
-#include<unordered_set>
-#include<vector>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
+/*
+
+You are given a string password.
+
+The strength of the password is calculated based on the following rules:
+
+1 point for each distinct lowercase letter ('a' to 'z').
+2 points for each distinct uppercase letter ('A' to 'Z').
+3 points for each distinct digit ('0' to '9').
+5 points for each distinct special character from the set "!@#$".
+Each character contributes at most once, even if it appears multiple times.
+
+Return an integer denoting the strength of the password.
+
+
+
+Example 1:
+
+Input: password = "aA1!"
+
+Output: 11
+
+Explanation:
+
+The distinct characters are 'a', 'A', '1' and '!'.
+Thus, the strength = 1 + 2 + 3 + 5 = 11.
+Example 2:
+
+Input: password = "bbB11#"
+
+Output: 11
+
+Explanation:
+
+The distinct characters are 'b', 'B', '1' and '#'.
+Thus, the strength = 1 + 2 + 3 + 5 = 11.​​​​​​​
+
+
+*/
 
 class Solution
 {
